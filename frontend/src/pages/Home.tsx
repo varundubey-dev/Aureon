5bc0c54e2c9433fc58dc10796b90dcc14146f61a
Varun Dubey <varundubey963@gmail.com>
2026-05-12 21:41:23 +0530

    chore: initialize frontend foundation with Vite and Tailwind

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        Aureon
+      </h1>
+    </div>
+  );
+}
